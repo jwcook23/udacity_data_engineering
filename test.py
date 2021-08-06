39d2e9ce1918966d7845419058779bfddac8842b
jwcook23 <30755439+jwcook23@users.noreply.github.com>
2021-08-06 19:49:44 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-
-conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-cur = conn.cursor()
-
-cur.execute('SELECT song_id, artist_id FROM songplays WHERE song_id IS NOT NULL')
-result = cur.fetchall()
-print(result)
