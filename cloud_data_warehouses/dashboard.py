21d20487e8eea27b97d96d99f6e9e797bf079b75
Jason Cook <jason.cook1989@gmail.com>
2021-11-29 07:02:45 -0500

    added hour historgram

--- cloud_data_warehouses/dashboard.py
+++ cloud_data_warehouses/dashboard.py
@@ -8,7 +8,7 @@ conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=
 cur = conn.cursor()
 
 # free vs paid 
-def get_level():
+def level():
     query = """
     WITH _total AS (
         SELECT
@@ -28,20 +28,22 @@ def get_level():
     chart = alt.Chart(data, title='Service Level')
     chart = chart.mark_bar()
     chart = chart.encode(
-        x=alt.X('percent:Q', stack='zero', axis=alt.Axis(format='%')),
-        y=alt.Y('level:N')
+        x=alt.X('percent', stack='zero', axis=alt.Axis(format='%')),
+        y=alt.Y('level')
     )
+    chart = chart.properties(height=100)
 
     text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white')
     text = text.encode(
-        x=alt.X('percent:Q', stack='zero'),
-        y=alt.Y('level:N'),
-        text=alt.Text('percent:Q', format='.0%')
+        x=alt.X('percent', stack='zero'),
+        y=alt.Y('level'),
+        text=alt.Text('percent', format='.0%')
     )
 
     return chart + text
 
-def get_timeline():
+def datetrend():
+    # TODO: verify ON time.start_time = songplays.start_time JOIN
     query = """
     SELECT
         time.day,
@@ -53,23 +55,72 @@ def get_timeline():
     """
     data = pd.read_sql(query, conn)
 
-    chart = alt.Chart(data)
+    chart = alt.Chart(data, title = 'Song Play Date Trend')
     chart = chart.mark_line()
     chart = chart.encode(
-        x='day:Q',
-        y='count:Q'
+        x='day',
+        y='count'
     )
+    chart = chart.properties(height=200)
 
     return chart
 
-# final dashboard
-level = get_level()
-level = level.properties(height=100)
+def hourbucket():
+    # TODO: verify ON time.start_time = songplays.start_time JOIN
+    query = """
+    SELECT
+        time.hour,
+        COUNT(songplays.songplay_id) AS count
+    FROM songplays
+    LEFT JOIN time
+        ON time.start_time = songplays.start_time
+    GROUP BY time.hour
+    """
+    data = pd.read_sql(query, conn)
 
-timeline = get_timeline()
-timeline = timeline.properties(height=200)
+    chart = alt.Chart(data, title='Song Play Hourly Distribution')
+    chart = chart.mark_bar()
+    chart = chart.encode(
+        x=alt.X('hour', bin=alt.BinParams(step=1), axis=alt.Axis(format='.0f')),
+        y='count'
+    )
+    chart = chart.properties(height=150)
 
-dashboard = level | timeline
+    return chart
+
+def location():
+
+    # derive state from location extrafter comma, first dash
+    # extract state from text after common then before dash
+    # ex: Minneapolis-St. Paul-Bloomington, MN-WI -> MN
+    query = """"""
+    # TODO: does this plot states using initials?
+    from vega_datasets import data
+    states = alt.topo_feature(data.us_10m.url, 'states')
+    source = data.population_engineers_hurricanes.url
+    variable_list = ['population', 'engineers', 'hurricanes']
+
+    alt.Chart(states).mark_geoshape().encode(
+        alt.Color(alt.repeat('row'), type='quantitative')
+    ).transform_lookup(
+        lookup='id',
+        from_=alt.LookupData(source, 'id', variable_list)
+    ).properties(
+        width=500,
+        height=300
+    ).project(
+        type='albersUsa'
+    ).repeat(
+        row=variable_list
+    ).resolve_scale(
+        color='independent'
+    )
+
+# position each chart into final dashboard
+# TODO: associated played songs (because you also listed to)
+# TODO: trending songs, trending artists
+# TODO: text over heatmap artist country? https://altair-viz.github.io/gallery/layered_heatmap_text.html
+dashboard = location() #level() | (hourbucket() & datetrend())
 
 # open dashboard in webbrowser
 url = os.path.join(os.getcwd(),'dashboard.html')
