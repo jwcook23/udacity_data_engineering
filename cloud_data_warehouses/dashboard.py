e22a6523a01acc49d4abfeb5b85df57fc67722b8
Jason Cook <jason.cook1989@gmail.com>
2021-12-03 06:49:37 -0500

    location and users serperate color scale

--- cloud_data_warehouses/dashboard.py
+++ cloud_data_warehouses/dashboard.py
@@ -3,7 +3,7 @@ import webbrowser
 import psycopg2
 import pandas as pd
 import altair as alt
-from vega_datasets import data
+from vega_datasets import data as vega_data
 
 conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
 cur = conn.cursor()
@@ -24,9 +24,9 @@ def level():
     FROM _total;
     """
 
-    data = pd.read_sql(query, conn)
+    source = pd.read_sql(query, conn)
 
-    chart = alt.Chart(data, title='Service Level')
+    chart = alt.Chart(source, title='Service Level')
     chart = chart.mark_bar()
     chart = chart.encode(
         x=alt.X('percent', stack='zero', axis=alt.Axis(format='%')),
@@ -34,7 +34,7 @@ def level():
     )
     chart = chart.properties(height=100)
 
-    text = alt.Chart(data).mark_text(dx=-15, dy=3, color='white')
+    text = alt.Chart(source).mark_text(dx=-15, dy=3, color='white')
     text = text.encode(
         x=alt.X('percent', stack='zero'),
         y=alt.Y('level'),
@@ -54,9 +54,9 @@ def datetrend():
         ON time.start_time = songplays.start_time
     GROUP BY day
     """
-    data = pd.read_sql(query, conn)
+    source = pd.read_sql(query, conn)
 
-    chart = alt.Chart(data, title = 'Song Play Date Trend')
+    chart = alt.Chart(source, title = 'Song Play Date Trend')
     chart = chart.mark_line()
     chart = chart.encode(
         x='day',
@@ -77,9 +77,9 @@ def hourbucket():
         ON time.start_time = songplays.start_time
     GROUP BY time.hour
     """
-    data = pd.read_sql(query, conn)
+    source = pd.read_sql(query, conn)
 
-    chart = alt.Chart(data, title='Song Play Hourly Distribution')
+    chart = alt.Chart(source, title='Song Play Hourly Distribution')
     chart = chart.mark_bar()
     chart = chart.encode(
         x=alt.X('hour', bin=alt.BinParams(step=1), axis=alt.Axis(format='.0f')),
@@ -112,34 +112,72 @@ def location():
     FROM _parsed
     GROUP BY abbr
     """
-    plays = pd.read_sql(query, conn)
-    states = alt.topo_feature(data.us_10m.url, feature='states')
+    source = pd.read_sql(query, conn)
+    states = alt.topo_feature(vega_data.us_10m.url, feature='states')
 
-    # get ANSI standard id field with abbr to match to us_10m dataset
-    ansi = pd.read_csv('https://www2.census.gov/geo/docs/reference/state.txt', sep='|')
-    ansi.columns = ['id', 'abbr', 'state', 'statens']
-    ansi = ansi[['id', 'abbr', 'state']]
-    ansi = ansi.merge(plays, left_on='abbr', right_on='abbr', how='right')
+    # get ANSI standard ID field with abbr to match to us_10m dataset
+    # state_id = pd.read_csv('https://www2.census.gov/geo/docs/reference/state.txt', sep='|')
+    # state_id.columns = ['id', 'abbr', 'state', 'statens']
+    # state_id = state_id[['id', 'abbr', 'state']]
+    # state_id.to_csv('state_id.csv', index=False)
+    state_id = pd.read_csv('state_id.csv')
 
-    base = alt.Chart(states).mark_geoshape(fill='lightgray', stroke='black', strokeWidth=0.5)
+    # add play count to ANSI standard ID
+    state_id = state_id.merge(source, left_on='abbr', right_on='abbr', how='right')
 
+    # create base map of US states to plot states with no play_count
+    base = alt.Chart(states).mark_geoshape(fill='white', stroke='black', strokeWidth=0.5)
+
+    # create outlines of states with heatmap of play_count
     chart = alt.Chart(states).mark_geoshape(stroke='black').encode(
-        color='play_count:Q'
+        color=alt.Color('play_count:Q', title='Play Count', scale=alt.Scale(scheme='lightmulti'))
     ).transform_lookup(
         lookup='id',
-        from_=alt.LookupData(ansi, 'id', ['play_count'])
+        from_=alt.LookupData(state_id, 'id', ['play_count'])
     ).properties(
         width=500,
         height=300
-    ).project('albersUsa')
+    )
+    # add appropriate zoom/view (projection)
+    chart = chart.project('albersUsa')
 
     return base + chart
+
+
+def top_users():
+
+    query = """
+    SELECT 
+        user_id,
+        COUNT(songplay_id) as playcount
+    FROM "songplays" 
+    GROUP BY user_id
+    """
+    source = pd.read_sql(query, conn)
+
+    # source = data.movies.url
+
+    # top 10
+    chart = alt.Chart(
+        source,
+    ).mark_bar().encode(
+        x=alt.X('user_id:N', sort='-y'),
+        y=alt.Y('playcount:Q'),
+        color=alt.Color('playcount:Q')
+    ).transform_window(
+        rank='rank(playcount)',
+        sort=[alt.SortField('playcount', order='descending')]
+    ).transform_filter(
+        (alt.datum.rank < 25)
+    )
+
+    return chart
 
 # position each chart into final dashboard
 # TODO: associated played songs (because you also listed to)
 # TODO: trending songs, trending artists
 # TODO: text over heatmap artist country? https://altair-viz.github.io/gallery/layered_heatmap_text.html
-dashboard = location() #level() | (hourbucket() & datetrend())
+dashboard = (level() & hourbucket() & datetrend()) | (location() & top_users()).resolve_scale(color='independent')
 
 # open dashboard in webbrowser
 url = os.path.join(os.getcwd(),'dashboard.html')
